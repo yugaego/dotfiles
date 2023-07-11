164d4828afafd887b321afbe2b648b7594439345
YugaEgo <yet@ego.team>
2023-07-11 17:55:08 +0300

    Improve ghpr.fish function

--- .config/fish/functions/ghpr.fish
+++ .config/fish/functions/ghpr.fish
@@ -1,8 +1,13 @@
 function ghpr -d "Pull GitHub PR to local branch"
+    set remote upstream
+    if set -q argv[2]
+        set remote "$argv[2]"
+    end
+    echo "Using remote: $remote"
     if set -q argv[1]
-        git fetch upstream pull/"$argv[1]"/head:ghpr
+        git fetch $remote pull/"$argv[1]"/head:ghpr
         git checkout ghpr
     else
-        "Enter GitHub Pull Request ID"
+        "Enter GitHub Pull Request ID [and a remote name]"
     end
 end
