7e76fee903c64cf3f18840e6a536e3ff84a6c8a2
YugaEgo <yet@ego.team>
2023-08-03 21:45:33 +0300

    Improve fish shell custom ghpr function

--- .config/fish/functions/ghpr.fish
+++ .config/fish/functions/ghpr.fish
@@ -1,13 +1,25 @@
-function ghpr -d "Pull GitHub PR to local branch"
+function ghpr -d "Fetch GitHub PR to local branch"
+    argparse 'h/help' 'id=' 'r/remote=' 'b/branch=' -- $argv
+    or return
+
+    if set -q _flag_help
+        or not set -q _flag_id
+        echo "ghpr --id [-r|--remote<upstream>] [-b|--branch<ghpr>]"
+        return 0
+    end
+
     set remote upstream
-    if set -q argv[2]
-        set remote "$argv[2]"
+    if set -q _flag_remote
+        set remote $_flag_remote
     end
-    echo "Using remote: $remote"
-    if set -q argv[1]
-        git fetch $remote pull/"$argv[1]"/head:ghpr
-        git checkout ghpr
-    else
-        "Enter GitHub Pull Request ID [and a remote name]"
+    echo "Use remote: $remote"
+
+    set branch ghpr
+    if set -q _flag_branch
+        set branch $_flag_branch
     end
+    echo "Create branch: $branch"
+
+    git fetch $remote pull/"$_flag_id"/head:"$branch"
+    git checkout $branch
 end
